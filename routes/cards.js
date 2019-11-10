a5ab1ccb32d688bad67105cd41ab81baec11261e
Your Name <you@example.com>
2019-11-11 01:50:15 +0500

    first

---
+++ routes/cards.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+const {cards} = require('../data/cards.js');
+module.exports = router;
+router.get('/cards',(req,res)=> {
+  res.send(cards);
+});
+
---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const routerUser = require('express').Router();
+
+const {users} = require('../data/users.js');
+module.exports = routerUser;
+routerUser.get('/users',(req,res)=> {
+  res.send(users);
+});
+routerUser.get('/users/:id',(req,res)=> {
+    const {id}=req.params;
+    const user = users.find(user =>{
+       return user.id === id 
+    })
+    if (user) {
+    
+        res.send(user)
+    }
+    else {
+        res.send({ "message": "Нет пользователя с таким id" })
+    }
+    
+        
+    });
