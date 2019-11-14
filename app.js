4e929c2e3be76305f453107990c6f8b6b1897b0f
Your Name <you@example.com>
2019-11-15 00:39:33 +0500

    6 commit

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-const express = require('express');
-const routes=require('./routes/cards')
-const routesUser=require('./routes/users')
+const express = require("express");
+const routes = require("./routes/cards");
+const routesUser = require("./routes/users");
+
 const { PORT = 3000 } = process.env;
-
 const app = express();
-app.use('/',routes)
-app.use('/',routesUser)
-
-
-
-
+app.use("/", routes);
+app.use("/", routesUser);
 app.listen(PORT, () => {
-    // Если всё работает, консоль покажет, какой порт приложение слушает
-    console.log(`App listening on port ${PORT}`)
-  })
-
-app.use(express.static(__dirname + '/public'));
-app.get('/*',(req,res)=> {
-    res.status('404').send({ "message": "Запрашиваемый ресурс не найден" });
-  });
-    
-
-
-
-
-
+});
+app.use(express.static(`${__dirname}/public`));
+app.get("/*", (req, res) => {
+  res.status("404").send({ message: "Запрашиваемый ресурс не найден" });
+});
